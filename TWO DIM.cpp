52d827e1b7f0c923368ca5afd4fd869961c3bbe8
jmgee <84143379+jmgee@users.noreply.github.com>
2026-01-26 14:25:57 +0800

    Refactor constants and variable names for clarity

--- TWO DIM.cpp
+++ TWO DIM.cpp
@@ -6,19 +6,19 @@ void computeTotals(double prices[][4], double totals[], int buyers);
 
 int main()
 {
-    const int BUYERS = 5;
-    const int ITEMS  = 4;
-    double prices[BUYERS][ITEMS];
-    double totals[BUYERS];
+    const int buyers = 5;
+    const int items  = 4;
+    double prices[buyers][items];
+    double totals[buyers];
     cout << "Enter four item prices for each buyer\n\n";
-    for (int i = 0; i < BUYERS; i++) {
+    for (int i = 0; i < buyers; i++) {
         cout << "Buyer #" << i + 1 << endl;
-        for (int j = 0; j < ITEMS; j++) {
+        for (int j = 0; j < items; j++) {
             cin >> prices[i][j];
         }
         cout << endl;
     }
-    computeTotals(prices, totals, BUYERS);
+    computeTotals(prices, totals, buyers);
     cout << fixed << setprecision(2);
     cout << setw(10) << "Buyer"
          << setw(12) << "Item 1"
@@ -27,9 +27,9 @@ int main()
          << setw(12) << "Item 4"
          << setw(15) << "Total Price" << endl;
     double grandTotal = 0;
-    for (int i = 0; i < BUYERS; i++) {
+    for (int i = 0; i < buyers; i++) {
         cout << setw(10) << "Buyer #" << i + 1;
-        for (int j = 0; j < ITEMS; j++) {
+        for (int j = 0; j < items; j++) {
             cout << setw(12) << prices[i][j];
         }
         cout << setw(15) << totals[i] << endl;
@@ -41,10 +41,10 @@ int main()
 
 void computeTotals(double prices[][4], double totals[], int buyers)
 {
-    const int ITEMS = 4;
+    const int items = 4;
     for (int i = 0; i < buyers; i++) {
         totals[i] = 0;
-        for (int j = 0; j < ITEMS; j++) {
+        for (int j = 0; j < items; j++) {
             totals[i] += prices[i][j];
         }
     }
